53b0543deabfef4d78d4dee8430c887dac451490
noasigan <noa.sigan2001@gmail.com>
2026-08-20 22:48:13 +0300

    Initial commit

--- .venv/api_check.py
+++ .venv/api_check.py
@@ -1,12 +1,1 @@
-import requests
-
-def test_login_success():
-    response = requests.post(
-                "https://api.example.com/login",
-                 json={
-                     username: "admin",
-                     password: "1234"
-
-                 }
-    )
-    assert response.status_code == 200
+res
